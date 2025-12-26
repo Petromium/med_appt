d2e502e8ae6cbf11c79ba8726837bf201a5fbfd2
Petromium <mohammad.al.jarad@petromium.com>
2025-12-26 10:44:04 -0500

    Updated deployment configuration and redeployed application

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const path = require('path');  // ADD THIS LINE
 const connectToMongo = require('./db');
 const app = express();
-
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
 const PORT = process.env.PORT || 8181;
-
 
 // Middleware
 app.use(express.json());
@@ -18,16 +17,18 @@ app.use(cors());
 // Connect to MongoDB
 connectToMongo();
 
-// Routes
+// API Routes
 app.use('/api/auth', require('./routes/auth'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// All other routes serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-
-  // Start the server
+// Start the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
